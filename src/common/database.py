0bfcac5626732f006a4739161e943d9543805ee4
AidenTran15 <aidenkiettran@gmail.com>
2019-12-24 16:23:35 +1100

    [Update] Add models for flask

--- src/common/database.py
+++ src/common/database.py
@@ -1,1 +1,23 @@
-import 
+import pymongo
+
+class Database(object):
+    URI = "mongodb://127.0.0.1:27017"
+    DATABASE = None
+
+    @staticmethod
+    def initailize():
+        client = pymongo.MongoClient(Database.URI)
+        Database.Database = client['fullstack']
+
+    @staticmethod
+    def insert(collection, data):
+        Database.DATABASE[collection].insert(data)
+
+    @staticmethod
+    def find(collection, data):
+        return Database.DATABASE[collection].find(query)
+
+    @staticmethod
+    def find_one(collection, data):
+        return Database.DATABASE[collection].find_one(query)
+        
