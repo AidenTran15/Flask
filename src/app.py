20c89605d0f3a05436442766aecaeda68b5f1d8a
AidenTran15 <aidenkiettran@gmail.com>
2019-12-30 16:30:20 +1100

    Displaying a lists of posts

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, session, request
 from src.models.user import User
 from src.common.database import Database
+from src.models.blog import Blog
 
 
 app = Flask(__name__) #'__main__'
@@ -54,7 +55,13 @@ def user_blogs(user_id=None):
     blogs = user.get_blogs()
 
     return render_template("user_blogs.html", blogs=blogs, email=user.email)
-     
+
+@app.route('/posts/<string:blog_id>')
+def blog_posts(blog_id):
+    blog = Blog.from_mongo(blog_id)
+    posts = blog.get_posts()
+
+    return render_template('posts.html', posts=posts, blog_title=blog.title, blog_id=blog._id)
 
 if __name__ == '__main__':
     app.run(port=1511)
